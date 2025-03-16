f79f3dc54db17228924cbba6128720737e9d5235
GerardZhou <dorey1200@gmail.com>
2025-03-16 15:21:10 -0500

    finished logout authRoute controller backend

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -91,7 +91,17 @@ export const login = async (req, res) => {
 };
 
 export const logout = async (req, res) => {
-  res.json({
-    data: "Hit the logout endpoint",
-  });
+  try {
+    res.cookie("jwt", "", { maxAge: 0 });
+    res.status(200).json({ message: "Logged out successfully" });
+  } catch (error) {
+    console.log("Logout controller Error: ", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+export const getMe = async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id);
+  } catch (error) {}
 };
