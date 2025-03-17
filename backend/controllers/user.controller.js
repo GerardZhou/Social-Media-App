a2a58579c608411abdb4c27323520d8c524ffd08
GerardZhou <dorey1200@gmail.com>
2025-03-17 17:39:50 -0500

    follow/unfollow functionality backend

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -14,3 +14,35 @@ export const getUserProfile = async (req, res) => {
     res.status(500).json({ error: "Internal server error" });
   }
 };
+export const followUnfollowUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const userToModify = await User.findById(id);
+    const currUser = await User.findById(req.user._id);
+
+    if (id === req.user._id.toString()) {
+      return res
+        .status(400)
+        .json({ error: "You can't follow/unfollow yourself" });
+    }
+    if (!userToModify || !currUser) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    const isFollowing = currUser.following.includes(id);
+    if (isFollowing) {
+      // unfollow user
+      await User.findByIdAndUpdate(id, { $pull: { followers: req.user._id } });
+      await User.findByIdAndUpdate(req.user._id, { $pull: { following: id } });
+      res.status(200).json({ message: "User unfollowed" });
+    } else {
+      // follow user
+      await User.findByIdAndUpdate(id, { $push: { followers: req.user._id } });
+      await User.findByIdAndUpdate(req.user._id, { $push: { following: id } });
+      // send notification to user
+      res.status(200).json({ message: "User followed" });
+    }
+  } catch (error) {
+    console.log("followUnfollowUser Error: ", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
