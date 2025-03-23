d85332bc1b1e09c2c1783c8d216484a3cf994ee9
GerardZhou <dorey1200@gmail.com>
2025-03-23 17:32:51 -0500

    debugged controller toues issue

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,6 +1,6 @@
 import express from "express";
-import { protectRoute } from "../middleware/protectRoute";
-
+import { protectRoute } from "../middleware/protectRoute.js";
+import { createPost } from "../controllers/post.controller.js";
 const router = express.Router();
 
 router.post("/create", protectRoute, createPost);
