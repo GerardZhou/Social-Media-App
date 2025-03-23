05dc15c32540e3a196e3d0abb5435369f8206bbb
GerardZhou <dorey1200@gmail.com>
2025-03-23 16:42:13 -0500

    createPost route implemented in backend

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,5 +1,10 @@
 import express from "express";
+import { protectRoute } from "../middleware/protectRoute";
 
 const router = express.Router();
 
+router.post("/create", protectRoute, createPost);
+// router.post("/like/:id", protectRoute, likeUnlikePost);
+// router.post("/comment/:id", protectRoute, commentOnPost);
+// router.delete("/", protectRoute, deletePost);
 export default router;
