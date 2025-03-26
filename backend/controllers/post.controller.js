29368912becb4d5ea0759844346b3689100a1f3b
GerardZhou <dorey1200@gmail.com>
2025-03-25 22:45:12 -0500

    debugged and tested notification through mongoDB and postman

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -1,6 +1,6 @@
 import User from "../models/user.model.js";
 import Post from "../models/post.model.js";
-import notification from "../models/notification.model.js";
+import Notification from "../models/notification.model.js";
 import { v2 as cloudinary } from "cloudinary";
 
 export const createPost = async (req, res) => {
