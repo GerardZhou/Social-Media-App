5db45de554ac30acb635d243648dea578e6b63ea
GerardZhou <dorey1200@gmail.com>
2025-03-31 21:18:20 -0500

    debugged and finished delete/get notification route controller backend. tested using postman

--- backend/controllers/notification.controller.js
+++ backend/controllers/notification.controller.js
@@ -11,8 +11,18 @@ export const getNotifications = async (req, res) => {
     await Notification.updateMany({ to: userId }, { read: true });
     res.status(200).json(notifications);
   } catch (error) {
-    console.error("Error fetching notifications: ", error);
+    console.log("Error fetching notifications: ", error);
     res.status(500).json({ error: "Internal server error" });
   }
 };
-export const deleteNotifications = async (req, res) => {};
+export const deleteNotifications = async (req, res) => {
+  try {
+    const userId = req.user._id;
+
+    await Notification.deleteMany({ to: userId });
+    res.status(200).json({ message: "Notifications deleted successfully" });
+  } catch (error) {
+    console.log("Error deleting notifications: ", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
