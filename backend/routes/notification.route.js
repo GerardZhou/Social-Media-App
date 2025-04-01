a2c37301e73016f3e17f12c8f3ef0f5b89c3e1a5
GerardZhou <dorey1200@gmail.com>
2025-03-31 20:09:29 -0500

    created getNotification function to read in update notifications when they are read by the user

--- backend/routes/notification.route.js
+++ backend/routes/notification.route.js
@@ -4,7 +4,7 @@ import { protectRoute } from "../middleware/protectRoute";
 import {
   getNotifications,
   deleteNotifications,
-} from "../controllers/notification.controller";
+} from "../controllers/notification.controller.js";
 const router = express.Router();
 
 router.get("/", protectRoute, getNotifications);
