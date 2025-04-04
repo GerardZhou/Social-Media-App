7495f2b0afd25d3d82f32592e3fc86652f34ad89
GerardZhou <dorey1200@gmail.com>
2025-04-03 22:44:37 -0500

    setting up daisyUI themes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,14 @@
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <p className="text-5xl text-red-500 bg-blue-300">Frontend part</p>
+      <button className="btn btn-soft">Default</button>
+      <button className="btn btn-soft btn-primary">Primary</button>
+      <button className="btn btn-soft btn-secondary">Secondary</button>
+      <button className="btn btn-soft btn-accent">Accent</button>
+      <button className="btn btn-soft btn-info">Info</button>
+      <button className="btn btn-soft btn-success">Success</button>
+      <button className="btn btn-soft btn-warning">Warning</button>
+      <button className="btn btn-soft btn-error">Error</button>
     </>
   );
 }
