483d3cafd2837b80180dce3c4b17d3474e8b0ec9
GerardZhou <dorey1200@gmail.com>
2025-04-03 22:49:47 -0500

    used data-theme black and BrowserRouter React

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,12 @@
 function App() {
   return (
-    <>
-      <button className="btn btn-soft">Default</button>
-      <button className="btn btn-soft btn-primary">Primary</button>
-      <button className="btn btn-soft btn-secondary">Secondary</button>
-      <button className="btn btn-soft btn-accent">Accent</button>
-      <button className="btn btn-soft btn-info">Info</button>
-      <button className="btn btn-soft btn-success">Success</button>
-      <button className="btn btn-soft btn-warning">Warning</button>
-      <button className="btn btn-soft btn-error">Error</button>
-    </>
+    <div className="flex max-w-6xl mx-auto">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </div>
   );
 }
 
