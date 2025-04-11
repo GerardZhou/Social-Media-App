37416a87b117ec75d530329604533c8c5c0d9179
GerardZhou <dorey1200@gmail.com>
2025-04-10 21:56:07 -0500

    signup page authentification in react frontend -> connecting frontend and backend

--- frontend/src/pages/auth/signup/SignUpPage.jsx
+++ frontend/src/pages/auth/signup/SignUpPage.jsx
@@ -7,13 +7,44 @@ import { MdOutlineMail } from "react-icons/md";
 import { FaUser } from "react-icons/fa";
 import { MdPassword } from "react-icons/md";
 import { MdDriveFileRenameOutline } from "react-icons/md";
-
+import { useMutation, useQuery } from "@tanstack/react-query";
 const SignUpPage = () => {
   const [formData, setFormData] = useState({
     email: "",
     username: "",
     fullName: "",
     password: "",
+  });
+
+  const { mutate, isError, isPending, error } = useMutation({
+    mutationFn: async ({ email, username, fullName, password }) => {
+      try {
+        const res = await fetch("http://localhost:5000/api/auth/signup", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email,
+            username,
+            fullName,
+            password,
+          }),
+        });
+
+        if (!res.ok) {
+          throw new Error("Something went wrong");
+        }
+        const data = await res.json();
+        if (data.error) {
+          throw new Error(data.error);
+        }
+        console.log(data);
+        return data;
+      } catch (error) {
+        console.log(error);
+      }
+    },
   });
 
   const handleSubmit = (e) => {
@@ -24,8 +55,6 @@ const SignUpPage = () => {
   const handleInputChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
-
-  const isError = false;
 
   return (
     <div className="max-w-screen-xl mx-auto flex h-screen px-10">
