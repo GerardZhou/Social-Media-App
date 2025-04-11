cbe7942925cbf56a30f2d5cf7a5c57cda0f59f73
GerardZhou <dorey1200@gmail.com>
2025-04-10 21:32:52 -0500

    query client provider frontend mainjsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,11 +4,16 @@ import "./index.css";
 import App from "./App.jsx";
 import daisyui from "daisyui";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>
 );
