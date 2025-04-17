c0910916fc2d87ca532bbb740a8b5e3af35d7e0b
GerardZhou <dorey1200@gmail.com>
2025-04-17 16:30:58 -0500

    avoiding refetching on window focus + adding query Keys

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,10 +5,17 @@ import App from "./App.jsx";
 import daisyui from "daisyui";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import ReactDOM from "react-dom/client";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
-createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
